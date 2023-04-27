7d0f67a5fb7c709e7b090439d0775b1b2f47b738
Christoff <vdwstoffel@gmail.com>
2023-04-27 21:32:34 +0200

    destructure props

--- code/javascript/React/src/components/PropsExample.jsx
+++ code/javascript/React/src/components/PropsExample.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 function PropsExample(props) {
+  const { title } = props;
+
   return (
     <div className="note">
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
     </div>
   );
 }
