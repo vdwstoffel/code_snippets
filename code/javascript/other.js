e5744bf321c2c52659dca0136a1f2dc3e6385105
Christoff <vdwstoffel@gmail.com>
2023-04-30 21:38:52 +0200

    add libs to other.js

--- code/javascript/other.js
+++ code/javascript/other.js
@@ -1,13 +1,48 @@
 "use strict";
 
 /*
-https://www.npmjs.com/package/uuid
-npm install uuid
+A Collection of usefull modules
 */ 
-console.log('UUID')
-const { v4: uuidv4 } = require('uuid');
-console.log(uuidv4());
 
+/***************
+***** UUID *****
+****************/
+/* To create a random UUID... */ 
+const { v4: uuidv4 } = require('uuid');         // npm install uuid
+console.log(uuidv4()); // b8dff6a8-e942-417f-ade7-b62479d47c85
 
+/*****************
+***** Bcrypt *****
+******************/
+/* A library to help you hash passwords. */
+const bcrypt = require("bcrypt");               // npm i bcrypt
 
+const saltRounds = 12;
+const myPassword = "Stoffel";
 
+const storePassword = async () => {
+  const hashedPW = await bcrypt.hash(myPassword, saltRounds);
+  return hashedPW;
+};
+
+const checkPassword = async (plain, hash) => {
+  const result = await bcrypt.compare(plain, hash);
+  return result;
+};
+
+storePassword().then((hashedPW) => {
+  console.log(hashedPW);
+  checkPassword(myPassword, hashedPW).then((result) => {
+    console.log(result);
+  });
+});
+
+/*****************
+***** dotenv *****
+******************/
+/*  module that loads environment variables
+*   Create a .env file in the root of your project
+*   S3_BUCKET="YOURS3BUCKET"
+*   console.log(process.env)
+*/
+require('dotenv').config();                  // npm i dotenv       
