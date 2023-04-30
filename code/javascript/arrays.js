1839ef1f964d364394e0785e29639f14058cebec
Christoff <vdwstoffel@gmail.com>
2023-04-30 16:16:12 +0200

    update arrays

--- code/javascript/arrays.js
+++ code/javascript/arrays.js
@@ -8,3 +8,15 @@ console.log(names); // [ 'John', 'Mary' ]
 const fruit = ["apple", 'pear', 'orange'];
 const all = [...fruit, 'grapes'];
 console.log(all); // [ 'apple', 'pear', 'orange', 'grapes' ]
+
+// map example
+const cars = ['BMW', 'Volvo', 'Saab'];
+console.log(cars.map(x => x.toLocaleUpperCase)); // [ 'BMW', 'VOLVO', 'SAAB' ]
+
+// filter example
+const numbers = [1, 2, 3, 4, 5];
+const filtered = numbers.filter(x => x > 3); // [ 4, 5 ]
+
+// reduce example
+const nums = [1, 2, 3, 4, 5];
+const sum = nums.reduce((a, b) => a + b, 0); // 15
