abc0c0546eaeede929614bdc81d1e927b2ed79bd
Stoffel <vdwstoffel@gmail.com>
2023-07-30 12:17:08 +0200

    update message box

--- python/pyqt6/message_box_example.py
+++ python/pyqt6/message_box_example.py
@@ -1,6 +1,8 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox, QPushButton
 import sys
 
+# For other messages boxes
+# https://www.pythontutorial.net/pyqt/pyqt-qmessagebox/
 class QMessageBoxExample(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -10,11 +12,14 @@ class QMessageBoxExample(QMainWindow):
     def show_message(self):
 
         # Create a message box
-        message_box = QMessageBox()
-        message_box.setText('Hello, world!')
-
-        # Show the message box
-        message_box.exec()
+        message_box = QMessageBox.question(self, 'Confirmation', "Do you accept", QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+        
+        # Check what is clicked
+        if message_box == QMessageBox.StandardButton.Yes:
+            print("User Clicked Yes")
+        
+        if message_box == QMessageBox.StandardButton.No:
+            print("User Clicked No")
 
 if __name__ == '__main__':
     app = QApplication([])
